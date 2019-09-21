ea39a5533f8e7df2303660874e936ede783d7c28
liuhao <823232600@qq.comm>
2019-09-21 11:27:39 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
-import { Link  } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import routes from './routes';
 
